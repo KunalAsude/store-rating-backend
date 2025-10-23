2ede355c9787193cb2c43cae4d1786a92b22672e
Kunal Asude <143272678+KunalAsude@users.noreply.github.com>
2025-10-23 19:04:43 +0530

    Add comment for class injectability
    Add a comment to indicate the class is injectable.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-
+//make class injectable
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
